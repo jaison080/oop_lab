14825f61797ed8e19ff21bdeed6045ba63f92e30
Jaison Dennis <jaisondennis080@gmail.com>
2021-12-01 15:23:44 +0530

    Frequency.java

--- Frequency.java
+++ Frequency.java
@@ -1,23 +1,24 @@
 import java.io.*;
 import java.util.Scanner;
-public class Frequency
+public class Program7
 {
 	public static void main(String args[])
 	{
-		int i,l,count=0;
+		String str,str1;
 		char c;
-		String str;
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter the string:");
+		int l,i,count=0;
+		System.out.println("Enter the string");
 		str=in.nextLine();
-		System.out.println("Enter the character:");
+		str1=str.toLowerCase();
+		System.out.println("Enter the character");
 		c=in.next().charAt(0);
 		l=str.length();
 		for(i=0;i<l;i++)
 		{
-			if(str.charAt(i)==c)
+			if(str1.charAt(i)==c)
 				count++;
 		}
-		System.out.println("Frequency of charcter "+c+" : "+count);
+		System.out.println("Frequency of character "+c+" = "+count);
 	}
-}						
+}
