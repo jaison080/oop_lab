8ae3d725ef44d3d174e074f27fbc12a1722b9266
jaison080 <jaisondennis080@gmail.com>
2022-04-22 14:30:57 +0530

    Programs Updated

--- Num/Num.java
+++ Num/Num.java
@@ -1,9 +1,6 @@
-import java.io.*;
-public class Num
-{
-	public static void main(String args[])
-	{
-		int num; 
+public class Num {
+	public static void main(String args[]) {
+		int num;
 		num = 100;
 		System.out.println("This is num: " + num);
 		num = num * 2;
