dda16e55c45c1c854aea871e1ba622307d33e4e7
jaison080 <jaisondennis080@gmail.com>
2022-04-26 08:02:06 +0530

    Updated Example Program

--- Example/Example.java
+++ Example/Example.java
@@ -4,7 +4,7 @@ Call this file "Example.java".
 */
 class Example {
     // Your program begins with a call to main().
-    public static void main(String ags[]) {
+    public static void main(String args[]) {
         System.out.println("This is a simple Java program.");
     }
 }
